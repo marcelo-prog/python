5471179fc8bfb24cc3218b20643b8a59c8f7bf1a
MARCELO GOMES CASTANHO <71048436+marcelo-prog@users.noreply.github.com>
2021-01-12 21:21:34 -0300

    12/01/21

---
+++ python.py
@@ -0,0 +1,9 @@
+a = float(input('Digite o preço da laranja'))
+b = float (input('Digite o preço da banana'))
+if a <= 5.00 or b <= 5.00:
+    print('Compre laranja e banana')
+
+        else :
+            print('Não compre nada.')
+
+
